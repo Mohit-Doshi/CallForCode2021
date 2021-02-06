48e1cc5ebe32059d3422de3a7df61fc96e538e8c
Mohit-Doshi <mohit.dosi@gmail.com>
2021-02-06 08:55:02 -0700

    Styling for a horizontal abr

--- multi-page-app-with-react-master/src/components/Menu.js
+++ multi-page-app-with-react-master/src/components/Menu.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import style from './Menu.css';
+import './Menu.css';
 
 export default class Menu extends Component {
 
   render() {
     return (
-
-        <ul className={style.Menu}>
+      <div class="nav">
+        <ul>
           <li><a href="/index.html">Home</a></li>
           <li><a href="/products/product-1.html">Product</a></li>
           <li><a href="/contact.html">Results?</a></li>
           <li><a href="/App.html">App</a></li>
           <li><a href="/rewards.html">Rewards</a></li>
         </ul>
-        
+      </div>
     );
   }
 }
