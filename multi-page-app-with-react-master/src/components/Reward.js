fa82b1a45e6a0c81183b7f92fae4c22013ed774e
Mohit Doshi <mohit.dosi@gmail.com>
2021-02-06 14:11:39 -0700

    API call

--- multi-page-app-with-react-master/src/components/Reward.js
+++ multi-page-app-with-react-master/src/components/Reward.js
@@ -5,11 +5,34 @@ import Coupon from '../components/Coupon';
 
 export default class Reward extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      charts: [] 
+    };
+  }
+
+  
+
+  componentDidMount() {
+    fetch('https://us-south.functions.appdomain.cloud/api/v1/web/mohit.dosi%40gmail.com_dev/default/FetchCharities')
+    .then((response) => response.json())
+    .then(booksList => {
+        this.setState({ charts: booksList });
+        console.log(this.state.charts);
+    });
+}
+
 
 render() {
 
 
     let points = localStorage.getItem("ctcpoints");
+
+
+    // perform GET request here
+
+
 
     return (
       // render logic here
@@ -22,6 +45,8 @@ render() {
             <Coupon />
         </div>
         <div id="charities">
+
+
 
         </div>
 
