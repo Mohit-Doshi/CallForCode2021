6328412bf617982379ba782c6ae8987014f87f03
Mohit-Doshi <mohit.dosi@gmail.com>
2021-02-06 03:13:29 -0700

    Fixed linking with App page

--- multi-page-app-with-react-master/src/pages/App.js
+++ multi-page-app-with-react-master/src/pages/App.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Uploader from 'components/Uploader';
 
-ReactDOM.render(<Uploader />, document.getElementById('menu'));
+ReactDOM.render(<Uploader />, document.getElementById('app'));
